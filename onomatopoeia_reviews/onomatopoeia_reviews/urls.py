381ed86ebe6300dc07ba59b63503d54b2ae862b9
Ernesto Jiménez <ernesto@bedu.org>
2019-03-30 14:57:36 -0600

    class

--- onomatopoeia_reviews/onomatopoeia_reviews/urls.py
+++ onomatopoeia_reviews/onomatopoeia_reviews/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 from django.http import HttpResponse
 
-from reviews.views import index, movie_detail
+from reviews.views import index, movie_detail, add_review
 
 
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('movie-detail/<movie_id>/', movie_detail, name='movie-detail'),
+    path('add-review/<movie_id>/', add_review, name='add-review'),
 ]
--- onomatopoeia_reviews/reviews/views.py
+++ onomatopoeia_reviews/reviews/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from reviews.models import Movie
+from reviews.forms import ReviewForm
 
 
 
@@ -13,8 +17,21 @@ def index(request):
 
 
 def movie_detail(request, movie_id):
-    movie = Movie.objects.get(pk=movie_id)
+    movie = get_object_or_404(Movie, pk=movie_id)
+    review_form = ReviewForm()
     context = {
-        'movie': movie
+        'movie': movie,
+        'review_form': review_form
     }
     return render(request, 'movie-detail.html', context)
+
+@require_POST
+def add_review(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+        review = form.save(commit=False)
+        review.movie = movie
+        review.save()
+        return HttpResponseRedirect(reverse('movie-detail', args=[movie_id]))
+
